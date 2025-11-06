60b470fa6debe06a4b86654744d29dcac3dc2ca8
Dylane-Agoka <dylaneagoka47@gmail.com>
2025-11-06 06:47:01 +0100

    write the TotalCost component

--- src/components/ConferenceEvent/ConferenceEvent.jsx
+++ src/components/ConferenceEvent/ConferenceEvent.jsx
@@ -50,14 +50,69 @@ const ConferenceEvent = () => {
     }
   };
 
+  const totalCosts = {
+    venue: venueTotalCost,
+    av: avTotalCost,
+    meals: mealsTotalCost,
+  };
+
   const getItemsFromTotalCost = () => {
     const items = [];
+    venueItems.forEach((item) => {
+      if (item.quantity > 0) {
+        items.push({ ...item, type: 'venue' });
+      }
+    });
+    avItems.forEach((item) => {
+      if (item.quantity > 0 && !items.some((i) => i.name === item.name && i.type === 'av')) {
+        items.push({ ...item, type: 'av' });
+      }
+    });
+    mealsItems.forEach((item) => {
+      if (item.selected) {
+        const itemForDisplay = { ...item, type: 'meals' };
+        if (item.numberOfPeople) {
+          itemForDisplay.numberOfPeople = numberOfPeople;
+        }
+        items.push(itemForDisplay);
+      }
+    });
+    return items;
   };
 
   const items = getItemsFromTotalCost();
 
   const ItemsDisplay = ({ items }) => {
-
+    console.log(items);
+    return <>
+      <div className="display_box1">
+        {items.length === 0 && <p>No items selected</p>}
+        <table className="table_item_data">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Unit Cost</th>
+              <th>Quantity</th>
+              <th>Subtotal</th>
+            </tr>
+          </thead>
+          <tbody>
+            {items.map((item, index) => (
+              <tr key={index}>
+                <td>{item.name}</td>
+                <td>${item.cost}</td>
+                <td>
+                  {item.type === "meals" || item.numberOfPeople ? ` For ${numberOfPeople} people` : item.quantity}
+                </td>
+                <td>
+                  {item.type === "meals" || item.numberOfPeople ? `${item.cost * numberOfPeople}` : `${item.cost * item.quantity}`}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
   };
   const calculateTotalCost = (section) => {
     let totalCost = 0;
@@ -200,17 +255,17 @@ const ConferenceEvent = () => {
                 </div>
                 <div className="input-container venue_selection">
                   <label htmlFor="numberOfPeople"><h3>Number of People:</h3></label>
-                  <input type="number" className="input_box5" id="numberOfPeople" value={numberOfPeople} 
-                      onChange={(e) => setNumberOfPeople(parseInt(e.target.value))} 
-                      min="1"
+                  <input type="number" className="input_box5" id="numberOfPeople" value={numberOfPeople}
+                    onChange={(e) => setNumberOfPeople(parseInt(e.target.value))}
+                    min="1"
                   />
                 </div>
                 <div className="meal_selection">
                   {mealsItems.map((item, index) => (
                     <div className="meal_item" key={index} style={{ padding: 15 }}>
                       <div className="inner">
-                        <input type="checkbox" id={`meal_${index}`} 
-                          checked={ item.selected } onChange={() => handleMealSelection(index)}
+                        <input type="checkbox" id={`meal_${index}`}
+                          checked={item.selected} onChange={() => handleMealSelection(index)}
                         />
                         <label htmlFor={`meal_${index}`}> {item.name} </label>
                       </div>
@@ -223,7 +278,7 @@ const ConferenceEvent = () => {
             </div>
           ) : (
             <div className="total_amount_detail">
-              <TotalCost totalCosts={totalCosts} handleClick={handleToggleItems} ItemsDisplay={() => <ItemsDisplay items={items} />} />
+              <TotalCost totalCosts={totalCosts} ItemsDisplay={() => <ItemsDisplay items={items} />} />
             </div>
           )
         }
